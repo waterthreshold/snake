1c73f57690bdb04a414fb01a6ff30f272436af4c
waterthreshold <43978378+waterthreshold@users.noreply.github.com>
2019-01-04 10:08:57 +0800

    Add files via upload

---
+++ Snake/Snake/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace Snake
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.score_lbl = new System.Windows.Forms.Label();
+            this.time_lbl = new System.Windows.Forms.Label();
+            this.speed_lbl = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // score_lbl
+            // 
+            this.score_lbl.AutoSize = true;
+            this.score_lbl.Location = new System.Drawing.Point(66, 299);
+            this.score_lbl.Name = "score_lbl";
+            this.score_lbl.Size = new System.Drawing.Size(34, 12);
+            this.score_lbl.TabIndex = 1;
+            this.score_lbl.Text = "Score:";
+            // 
+            // time_lbl
+            // 
+            this.time_lbl.AutoSize = true;
+            this.time_lbl.Location = new System.Drawing.Point(164, 299);
+            this.time_lbl.Name = "time_lbl";
+            this.time_lbl.Size = new System.Drawing.Size(32, 12);
+            this.time_lbl.TabIndex = 2;
+            this.time_lbl.Text = "Time:";
+            // 
+            // speed_lbl
+            // 
+            this.speed_lbl.AutoSize = true;
+            this.speed_lbl.Location = new System.Drawing.Point(299, 299);
+            this.speed_lbl.Name = "speed_lbl";
+            this.speed_lbl.Size = new System.Drawing.Size(36, 12);
+            this.speed_lbl.TabIndex = 3;
+            this.speed_lbl.Text = "Speed:";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 30;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(9, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 18;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(450, 270);
+            this.tableLayoutPanel1.TabIndex = 4;
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.tableLayoutPanel1);
+            this.panel1.Location = new System.Drawing.Point(1, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(466, 280);
+            this.panel1.TabIndex = 6;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(479, 384);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.speed_lbl);
+            this.Controls.Add(this.time_lbl);
+            this.Controls.Add(this.score_lbl);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Snake";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label score_lbl;
+        private System.Windows.Forms.Label time_lbl;
+        private System.Windows.Forms.Label speed_lbl;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Snake/Snake/Form1.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Snake
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Start();
+            timer2.Start();
+        }
+        PictureBox [,]pic =new PictureBox[20,32];
+        int bait_x, bait_y;
+        Bitmap body = new Bitmap(15,15);
+        Bitmap bait = new Bitmap(15, 15);
+        Bitmap head = new Bitmap(15, 15);
+       
+        uint time = 0;
+        int body_count=3;
+        int score; int sum = 0;
+        int direction = 1;
+        int head_x, head_y;
+        int tail_x, tail_y;
+        Bitmap empty = new Bitmap(15, 15);
+        bool [,]cango=new bool[20,32];
+        List<int> body_x = new List<int>();
+        List<int> body_y = new List<int>();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            initialDraw();
+           
+        }
+
+        private void initialDraw()
+        {
+            Graphics g;
+            g = Graphics.FromImage(body);
+            g.DrawEllipse(new Pen(Color.Blue, 1), new Rectangle(0, 0, 8, 8));
+            g = Graphics.FromImage(head);
+            g.DrawEllipse(new Pen(Color.Red, 1), new Rectangle(0, 0,8, 8));
+            g = Graphics.FromImage(bait);
+            g.DrawRectangle(new Pen(Color.Red, 1),new Rectangle(0, 0, 8, 8));
+
+            for (int i = 0; i <= 19; i++)
+            {
+                for (int j = 0; j <= 31; j++)
+                {
+                    if (i == 0 || j == 0 || i == 19 || j == 31)
+                    {
+                        cango[i, j] = false;
+                    }
+                    else
+                    {
+                        cango[i, j] = true;
+                        pic[i, j] = new PictureBox();
+                        tableLayoutPanel1.Controls.Add(pic[i, j]);
+                    }
+
+                }
+            }
+            head_x = 12; head_y = 20;
+            body_x.Add(head_x);
+            body_y.Add(head_y);
+            tail_x = 12; tail_y = 23;
+            pic[head_x, head_y].BackgroundImage = head;
+            for (int i = 1; i <= body_count; i++)
+            {
+                if (i != body_count)
+                {
+                    body_x.Add(head_x);
+                    body_y.Add(head_y + i);
+                }
+                pic[head_x, head_y + i].BackgroundImage = body;
+            }
+            randombait();
+        }
+
+        private void randombait()
+        {
+            Random r = new Random();
+            bait_x = r.Next(17)+1;
+            bait_y = r.Next(29)+1;
+            if (body_x.Contains(bait_x) && body_y.Contains(bait_y))
+            {
+                randombait();
+            }
+            else
+            {
+                pic[bait_x, bait_y].BackgroundImage = bait;
+            }
+                
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            speed_lbl.Text = "Speed: " +timer1.Interval;
+            score_lbl.Text = "Score: " + sum;
+            if (body_count < 4 )
+            {
+                score = 2;
+                timer1.Interval = 1000;
+            }
+            else if (body_count >= 4&&body_count<7)
+            {
+                score = 3;
+                timer1.Interval = 700;
+            }
+            else if (body_count >= 7 && body_count < 10)
+            {
+                score = 5;
+                timer1.Interval = 300;
+            }
+            else if (body_count >= 10 && body_count < 13)
+            {
+                score = 7;
+                timer1.Interval = 100;
+            }
+            else
+            {
+                score = 11;
+                timer1.Interval = 70;
+            }
+            move();
+            
+        }
+        private void move()
+        {
+            try
+            {
+                int tmp_x;
+                int tmp_y;
+                bool flag = false;
+                if (direction == 1)
+                {
+                    --head_y;
+
+                }
+                else if (direction == 2)
+                {
+                    head_x++;
+
+                }
+                else if (direction == 3)
+                {
+                    head_y++;
+                }
+                else if (direction == 4)
+                {
+                    head_x--;
+                }
+                pic[head_x, head_y].BackgroundImage = head;
+                body_x.Add(head_x);
+                body_y.Add(head_y);
+                if (head_x == bait_x && head_y == bait_y)
+                {
+                    sum += score;
+                    flag = true;
+                }
+                else
+                {
+                    pic[tail_x, tail_y].BackgroundImage = empty;
+                }
+                for (int i = 1; i <= body_count; i++)
+                {
+                    if (i == body_count)
+                    {
+                        if (!flag)
+                        {
+                            pic[body_x[0], body_y[0]].BackgroundImage = body;
+                            tail_x = body_x[0];
+                            tail_y = body_y[0];
+                            body_x.RemoveAt(0);
+                            body_y.RemoveAt(0);
+                        }
+                        else
+                        {
+                            pic[body_x[0], body_y[0]].BackgroundImage = body;
+                            tmp_x = body_x[0];
+                            tmp_y = body_y[0];
+                            body_x.RemoveAt(0);
+                            body_y.RemoveAt(0);
+                            body_x.Add(tmp_x);
+                            body_y.Add(tmp_y);
+                            pic[tail_x, tail_y].BackgroundImage = body;
+                            body_count++;
+                            randombait();
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        pic[body_x[0], body_y[0]].BackgroundImage = body;
+                        tmp_x = body_x[0];
+                        tmp_y = body_y[0];
+                        body_x.RemoveAt(0);
+                        body_y.RemoveAt(0);
+                        body_x.Add(tmp_x);
+                        body_y.Add(tmp_y);
+                    }
+
+
+                }
+            }
+            catch (Exception e)
+            {
+                timer1.Stop();
+
+                if (MessageBox.Show("Play Again??", "Game Over!!", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    Application.Restart();
+                }
+                else
+                {
+                    Application.Exit();
+                }
+            }
+      
+           
+        }
+
+        
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up && direction != 2)
+            {
+                direction = 4;
+            }
+            else if (e.KeyCode == Keys.Down && direction != 4)
+            {
+                direction = 2;
+            }
+            else if (e.KeyCode == Keys.Left && direction != 3)
+            {
+                direction = 1;
+            }
+            else if (e.KeyCode == Keys.Right && direction != 1)
+            {
+                direction = 3;
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            time_lbl.Text = "Time : " + (++time);
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            g.Clear(Color.Black);
+        }
+    }
+}
